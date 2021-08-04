6cf5d7620ad517962e0b4e09ba4619cfd02f7428
Tanya Kalkuta <Tanyakalkuta@gmail.com>
2021-08-04 21:37:14 +0300

    Updates

---
+++ static/js/home-view.a58a2706.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[1],{129:function(e,o,t){},136:function(e,o,t){"use strict";t.r(o);t(0),t(129);var n=t(2);o.default=function(){return Object(n.jsx)("div",{className:"HomeContainer",children:Object(n.jsx)("h1",{className:"Home-title",children:"Your Phonebook"})})}}}]);
+//# sourceMappingURL=home-view.a58a2706.chunk.js.map
